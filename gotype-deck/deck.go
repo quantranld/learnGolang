9010ea27c618bf4612cfccbf3e81ff5046687581
quantran <quan@zoolooinfosys.com>
2019-07-02 23:13:04 +0800

    + manual way

--- gotype-deck/deck.go
+++ gotype-deck/deck.go
@@ -6,6 +6,6 @@ type deck []string
 
 func (d deck) handsome() {
 	for index, value := range d {
-		fmt.Print(index, value)
+		fmt.Println(index, value)
 	}
 }
--- gotype-deck/main.go
+++ gotype-deck/main.go
@@ -1,10 +1,17 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := deck{"card 1", "card 2", "card 3"}
-	fmt.Println("Hello world!")
 
 	cards.handsome()
 }
+
+// manual way
+// func main() {
+// 	cards := []string{"card 1", "card 2", "card 3"}
+// 	fmt.Println("Hello world!")
+
+// 	for _, val := range cards {
+// 		fmt.Println(val)
+// 	}
+// }
